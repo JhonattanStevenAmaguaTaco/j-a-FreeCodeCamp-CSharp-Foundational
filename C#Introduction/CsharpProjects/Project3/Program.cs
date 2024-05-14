566f728eaaad20fce0214629545c1eec510d4977
Jhonattan Steven Amagua Taco <jhonattan.amagua@epn.edu.ec>
2024-05-13 21:28:43 -0500

    New Commit

---
+++ C#Introduction/CsharpProjects/Project3/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+
+string [,] ourAnimals = new string[,]
+{
+    {"001","Cat","12","Black","cheerful"},
+    {"002","Dog","10","healty","cheerful"},
+    {"003","Cat","2","Brown","cheerful"},
+    {"004","Dog","1","green","cheerful"},
+    {"005","Cat","3","Orange","cheerful"},
+    {"006","Dog","5","Brown","cheerful"},
+    {"007","Horse","7","healty","cheerful"},
+    {"008","Cat","8","Orange","cheerful"},
+    {"009","Dog","6","healty","cheerful"},
+    {"010","Dog","4","Brown","cheerful"}
+};
+//menu 
+string entry;
+do 
+{
+    Console.WriteLine("Welcome to the Animal Farm");
+    Console.WriteLine("1. Display all animals");
+    Console.WriteLine("2. Add new Animals");
+    Console.WriteLine("3. Edit an animal age");
+    Console.WriteLine("4. Edit an animal personality");
+    Console.WriteLine("5. Display all cats from a specifiec pysical characteristic");
+    Console.WriteLine("6. Display all dogs from a specifiec pysical characteristic");
+    Console.WriteLine("7. Exit");
+    Console.WriteLine("Please enter a number from 1 to 6");
+    entry=Console.ReadLine();
+    
+    bool flag=int.TryParse(entry, out int intEntry);
+        if(!flag)
+    {
+        Console.WriteLine("Not a valid entry");
+    }
+    else if(intEntry>=1 && intEntry<=7)
+    {
+        switch( entry)
+        {
+            case "1":
+                Console.WriteLine("Display all animals");
+                break;
+            case "2":
+                Console.WriteLine("Add new Animals");
+                break;
+            case "3":
+                Console.WriteLine("Edit an animal age");
+                break;
+            case "4":
+                Console.WriteLine("Edit an animal personality");
+                break;
+            case "5":
+                Console.WriteLine("Display all cats from a specifiec pysical characteristic");
+                break;
+            case "6":
+                Console.WriteLine("Display all dogs from a specifiec pysical characteristic");
+                break;  
+            default:
+                Console.WriteLine("Exit");
+                break;  
+        }
+    }else
+    {
+        Console.WriteLine("Use  just numbers from 1 to 7");
+    }
+}
+while(entry!="7");
+
+
+   
+
+        
+
+
---
+++ WebApi/CsharpProjects/TestProjects/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, C#!");
+string[] fraudulentOrderIDs = new string[3];
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+//fraudulentOrderIDs[3] = "D000";
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+string[] fraudulentOrderIDs2 = { "A123", "B456", "C789" };
+
+Console.WriteLine($"First: {fraudulentOrderIDs2[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs2[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs2[2]}");
+
+Console.WriteLine($"length: {fraudulentOrderIDs2.Length}");
+///FOREACH
+string[] names={"Juan","Marcos","Bruno"};
+int count=0;
+foreach (string name in names){
+    count++;
+    Console.WriteLine($"el nombre en la posicion {count} es : {names[count-1]}");
+}
+string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string orderID in orderIDs)
+{
+    if (orderID.StartsWith("B"))
+    {
+        Console.WriteLine(orderID);
+    }
+}
